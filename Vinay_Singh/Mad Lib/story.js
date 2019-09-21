dcc9c6b96751d90639830c4b431a3108ee92ae28
Vinay1028 <54675330+Vinay1028@users.noreply.github.com>
2019-09-21 14:19:06 +0530

    Add files via upload

---
+++ Vinay_Singh/Mad Lib/story.js
@@ -0,0 +1,6 @@
+	var adjective = prompt("Please type a \"adjective:\"");
+	var verb = prompt("Please type a \"verb\"");
+	var  noun = prompt("please type a \"noun\"");
+	alert("All done! are you ready for the message?");
+	var message = "There once was a " + adjective + " programmmer who wanted to use javascript to " + verb +" the " + noun;
+	document.write(message);
