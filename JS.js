65a4fb3c4e7cb689f7e381b08a4b129209941767
akanksha4715 <46793326+akanksha4715@users.noreply.github.com>
2019-09-23 22:41:00 +0530

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,24 @@
+<html>
+<head>
+<body>
+"hi"
+
+<script>
+alert('hi');
+const vowel = ['a','e','i','o'];
+console.log(vowel[1]);
+vowel.push('s');
+
+//vowel.unshift('u');
+//vowel.pop();
+console.log(vowel.indexOf('a'));
+const person = {
+name : 'Akshat',
+hobbies : ['Music','Movie'],
+address : {
+street : 50
+city : 'gzb'
+</script>
+</body>
+</head>
+</html>
