c014160c7334ab88cb294b4760a11d0ff8fb1306
SsomyaSaxena <55494310+SsomyaSaxena@users.noreply.github.com>
2019-09-26 23:08:24 +0530

    Add files via upload

---
+++ SSOMYA/Date&Time.js
@@ -0,0 +1,5 @@
+var today = new Date();
+console.log("Today's complete date - ");
+console.log(today.getFullYear()+","+(today.getMonth()+1)+","+today.getDate())
+console.log("The present time is - ");
+console.log(today.getHours()+":"+today.getMinutes()+":"+today.getSeconds());
