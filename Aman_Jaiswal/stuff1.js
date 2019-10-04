62f7631f46264f423864a5abe8eabc84aef2d442
amanjaiswal777 <36677001+amanjaiswal777@users.noreply.github.com>
2019-10-04 23:30:12 +0530

    Delete stuff1.js

--- Aman_Jaiswal/stuff1.js
+++
@@ -1,9 +0,0 @@
-module.exports.counter = function(arr){
-  return 'There are' + arr.length + 'elements in the array';
-};
-
-module.exports.adder = function(a,b){
-  return `The sum of the two numbers is ${a+b}`;
-};
-
-module.exports.pi = 3.142;
